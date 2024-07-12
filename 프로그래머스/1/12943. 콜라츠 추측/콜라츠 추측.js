f640c17dd6cde94e8cd0541d1746b1cf57e0a5ec
yeji_kim <116805856+yejify@users.noreply.github.com>
2024-07-12 09:44:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,7 @@
+function solution(num) {
+        for (let i = 0; i < 500; i++){
+            if (num === 1) return i;
+            num = num % 2 === 0 ? num = num / 2 : num * 3 + 1;
+        }
+        return -1;
+    }
