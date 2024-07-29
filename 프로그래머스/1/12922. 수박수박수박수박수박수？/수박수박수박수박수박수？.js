1a05da1c5f6b9bcbc7bef884f405d61254d643dc
yeji_kim <116805856+yejify@users.noreply.github.com>
2024-07-29 09:29:24 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.44 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer =''
+    if(n%2===0){
+        for(let i = 0; i<n/2;i++){
+            answer += '수박'
+        }
+    }else if(n===1){
+        return '수'
+    }else{
+        for(let i = 0; i<Math.floor(n/2);i++){
+            answer += '수박'
+        }
+        answer+='수'
+    }
+    return answer
+}
